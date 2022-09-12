eec2efd24672bc6063ddc677866632187a4978b2
Leonard Norblad <norblad88@gmail.com>
2022-09-12 09:26:59 +0200

    Added data

---
+++ R/wiki_data.R
@@ -0,0 +1,18 @@
+#' Wiki_graph
+#' 
+#' A graph containing vertices and distances between them. Could be used in the dijkstra algorithm.
+#' 
+#' @format A \code{data.frame} of 18 rows and 3 colums.
+#' 
+#' @usage 
+#' 
+#' \describe{ 
+#' \item{v1}{First set of vertices}
+#' \item{v2}{Second set of vertices}
+#' \item{w}{Distance between vertices}
+#'  }
+#'  
+#' @references {Wikipedia contributors. (2022, August 21). Dijkstra's algorithm. In Wikipedia, The Free Encyclopedia. Retrieved 07:10, September 12, 2022, from \url{https://en.wikipedia.org/w/index.php?title=Dijkstra\%27s_algorithm&oldid=1105704278}}
+#' 
+#' @examples data(wiki_graph)
+"wiki_graph"
